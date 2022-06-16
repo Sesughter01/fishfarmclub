bb979c431e498a209b9e783136640fd07bc653ff
Sesughter01 <charles.avul@gmail.com>
2022-06-16 11:04:26 +0100

    correctly-changed-invest-button-to-link

--- src/containers/partners/Partners.jsx
+++ src/containers/partners/Partners.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {useState} from "react";
+import Axios from 'axios';
 import promotion_content from '../../components/promotions';
 import {ImgCard} from '../../components/';
 import './partners.css'
 import '../../components/img-card/imgcard.css'
 const Partners = () => {
+
+    const [myemail, setMail] = useState("0");
+
+    const addMail = () =>{
+        // console.log(name);
+        Axios.post('http://localhost:3002/make',{
+        email: myemail,
+       
+  
+  
+      }).then(()=>{
+         console.log("Success");
+      });
+     
+     };
     return (
         <div className="fishfarmclub__partners-container">
         
@@ -25,8 +42,9 @@ const Partners = () => {
                 <div className="fishfarmclub__partners-heading">
                     <h2>Learn how others are increasing their income streams and growing their capital.</h2>
                     <div className="fishfarmclub__partners-heading-input">
-                        <input type="email" placeholder='Enter your email' />
-                        <button type='button'>Join Our List</button>
+                        <input type="email" placeholder='Enter your email' onChange={(event)=> {
+                          setMail(event.target.value)}} />
+                        <button type='button' onClick={addMail}>Join Our List</button>
 
                     </div>
                     <div className="fishfarmclub__contact">
@@ -39,7 +57,10 @@ const Partners = () => {
                         </div>
                         <div className="fishfarmclub__getstarted">
                             <h1>Get started today!</h1>
-                            <button type='button'>INVEST NOW</button>
+                            <div className="invest">
+                                <a href='/categories'>INVEST NOW</a>
+                            </div>
+
                         </div>
                        
                     </div>
