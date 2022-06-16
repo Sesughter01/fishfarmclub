ac1d566488b575195af6e89346fb0f783dc2fe52
Sesughter01 <charles.avul@gmail.com>
2022-06-16 11:01:33 +0100

    changed-the-invest-and-package-buttons-to-links

--- src/containers/packages/Packages.jsx
+++ src/containers/packages/Packages.jsx
@@ -5,7 +5,9 @@ const Packages = () => {
     return (
         <div className='fishfarmclub__packages section__padding' id='packages'>
             <h1> Choose a Package to Invest</h1>
-            <button>Packages</button>
+            <div className="btn">
+                <a href='/categories'>Packages</a>
+            </div>
         </div>
     )
 }
