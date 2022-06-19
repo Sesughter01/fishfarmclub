1aec9be1b5cd5039df35ab2ae239c4e66ce33faa
Sesughter01 <charles.avul@gmail.com>
2022-06-19 15:32:31 +0100

    updated-ROIs-in-mypackagesjs

--- src/components/mypackages.js
+++ src/components/mypackages.js
@@ -6,9 +6,9 @@ const package_name= {
       package_name_1_details:"Amount:N250K - N450K ",
       package_name_2_details:"Amount:N500K - N5M  ",
       package_name_3_details:"Amount:N6M - N10M    ",
-      package_name_1_details_return:"Basic: Mini Pond ROI:20% Duration: 6 Months",
-      package_name_2_details_return:" Medium-Large ROI:25% Duration: 6 Months Auto Roll Over Incentive of 15KG Dried Fish Basket",
-      package_name_3_details_return:"Premium Investor ROI:30% Duration: 6 Months Auto Roll Over Incentive of 30KG Dried Fish Basket and Insurance cover"
+      package_name_1_details_return:"Basic: Mini Pond ROI:10% Duration: 6 Months",
+      package_name_2_details_return:" Medium-Large ROI:15% Duration: 6 Months Auto Roll Over Incentive of 15KG Dried Fish Basket",
+      package_name_3_details_return:"Premium Investor ROI:20% Duration: 6 Months Auto Roll Over Incentive of 30KG Dried Fish Basket and Insurance cover"
       // package_name_4_details:"Gold plan entitles you to 1000 fingerlings of catfish which will be managed for you until maturity"
 }
 
